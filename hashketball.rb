a6a939c751aa042ff4a07b586fdf5fd9dfa5ebdf
Neal JIn Oh <no4259@gmail.com>
2019-08-18 16:22:51 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -122,18 +122,151 @@ end
 
 
 
-def num_points_scored(name)
-  total = nil
+def num_points_scored(player_name)
   
-  game_hash.each do |location, team_data|
-    playstats = hash_info [:players]
-      playstats.each do |sports_stat|
-        if sports_stat[:name] == name
-          total = sports_stat[:points]
+  game_hash.each do |location, team|
+    team[:players].each do |name,data|
+        return data[:points] if player_name == name
+    end
+  end
+end
+
+
+def shoe_size(player_name)
+  
+  game_hash.each do |location, team|
+    team[:players].each do |name,data|
+        return data[:shoe] if player_name == name
+    end
+  end
+end
+
+
+def team_colors(name)
+  
+  game_hash.each do |location, team|
+    return game_hash[location][:colors] if team[:team_name] == name
+  end
+end
+
+
+def team_names
+  
+  game_hash.collect do |location, team|
+    team[:team_name]
+  end
+end
+
+
+def player_numbers(name)
+  
+  arr = []
+  
+  game_hash.each do |location, team|
+    if team[:team_name] == name
+      team[:players].each do |att, info|
+        arr << info[:number]
       end
     end
   end
-  total
+  return arr
+end
+
+
+def player_stats(name)
+  game_hash.each do |location, team|
+    team.each do |tel, int|
+      if int.include?(name)
+        return game_hash[location][tel][name]
+      end
+    end
+  end
+end
+
+
+
+def big_shoe_rebounds
+  
+  player_name = 0
+  shoe_size = 0
+  
+  game_hash.each do |location, team|
+    team[:players].each do |tel, int|
+      if int[:shoe] > shoe_size
+        shoe_size = int[:shoe]
+          player_name = int[:rebounds]
+        end
+      end
+    end
+    player_name
+end
+
+
+def most_points_scored
+  
+  score = 0
+  max_player = nil
+
+  game_hash.each do |location, team|
+    team[:players].each do |name,data|
+      
+      if data[:points] > score
+        score = data[:points]
+        max_player = name
+      end
+    end
+  end
+  return max_player
+end
+
+
+def winning_team
+  
+  score = 0
+  score2 = 0
+
+  game_hash[:home][:players].each do |name,data|
+    score += data[:points]
+  end
+  game_hash[:away][:players].each do |name,data|
+    score2 += data[:points]
+  end
+    if score > score2
+      game_hash[:home][:team_name]
+    else score < score2
+      game_hash[:away][:team_name]
+    end
+end
+
+
+def player_with_longest_name
+
+  person = nil
+
+  game_hash.each do |location, data|
+    data[:players].each do |name, value|
+      if name.length > 12
+        person = name
+      end
+    end
+  end
+  return person
+end
+
+
+def long_name_steals_a_ton?
+  name_wit = nil
+  steal_num = 10
+  
+  game_hash.each do |location, team|
+    team[:players].each do |name,data|
+      if data[:steals] > steal_num
+        steal_num = data[:steals]
+        name_wit = name
+      end
+    end
+  end
+  name_wit == player_with_longest_name
 end
 
 
